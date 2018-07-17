3fb18aabc41ae51e9e32f496ab3d758300961178
Matthew Lawson <matthew.lawson@ly.st>
2018-07-17 15:45:08 +0100

    Update versions for release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jinja-to-js',
-    version='3.2.2',
+    version='3.2.3',
     description="Turns Jinja templates into JavaScript functions that can be run in the browser.",
     keywords='jinja html javascript templating',
     author='Jon Bretman',
